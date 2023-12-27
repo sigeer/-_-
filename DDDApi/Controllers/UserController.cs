4032e3230bf8ca13c4c7deb05db9b4bd166cb69a
sigeer <1452069418@qq.com>
2023-12-27 10:46:23 +0800

    feat: getuserinfo

---
+++ DDDApi/Controllers/UserController.cs
@@ -0,0 +1,23 @@
+﻿using DDDApplication.Contract.Auth;
+using DDDApplication.Contract.Users;
+using Utility.Constants;
+
+namespace DDDApi.Controllers
+{
+    public class UserController: BaseApiController
+    {
+        readonly IUserService _userService;
+        readonly IAuthService _authService;
+
+        public UserController(IUserService userService, IAuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+        }
+
+        public ResponseModel<AuthUserInfoDto?> GetUserInfo()
+        {
+            return new ResponseModel<AuthUserInfoDto?>(_authService.GetUserInfo(UserId));
+        }
+    }
+}
